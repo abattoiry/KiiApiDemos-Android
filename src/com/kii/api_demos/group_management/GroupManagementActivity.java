87998e9da0836d198827248d38cf9c3ddf99cd27
Evan JIANG <firstfan@gmail.com>
2014-05-29 23:33:38 +0800

    Add sample code to fetch/create group bucket KiiObject

--- src/com/kii/api_demos/group_management/GroupManagementActivity.java
+++ src/com/kii/api_demos/group_management/GroupManagementActivity.java
@@ -5,6 +5,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
@@ -15,17 +16,27 @@ import com.kii.api_demos.AsyncTaskWithProgress;
 import com.kii.api_demos.R;
 import com.kii.api_demos.Utils;
 import com.kii.cloud.storage.Kii;
+import com.kii.cloud.storage.KiiBucket;
 import com.kii.cloud.storage.KiiGroup;
+import com.kii.cloud.storage.KiiObject;
 import com.kii.cloud.storage.KiiUser;
 import com.kii.cloud.storage.exception.GroupOperationException;
+import com.kii.cloud.storage.query.KiiQuery;
+import com.kii.cloud.storage.query.KiiQueryResult;
 
 import java.util.List;
 
 public class GroupManagementActivity extends FragmentActivity implements OnClickListener {
 
+    static final String BUCKET_NAME = "group_bucket";
+    static final String NOTE_FIELD_NAME = "note";
     KiiGroup currentGroup = null;
+    KiiGroup currentGroup2 = null;
     TextView groupIdTextView = null;
+    TextView groupId2TextView = null;
     EditText usernameField;
+    EditText contentField;
+    KiiObject group_content = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +51,12 @@ public class GroupManagementActivity extends FragmentActivity implements OnClick
         findViewById(R.id.select_owned_group).setOnClickListener(this);
         findViewById(R.id.add_to_group).setOnClickListener(this);
         findViewById(R.id.list_group_members).setOnClickListener(this);
+        findViewById(R.id.set_group_content).setOnClickListener(this);
+        findViewById(R.id.select_belonged_group).setOnClickListener(this);
         groupIdTextView = (TextView) findViewById(R.id.group_id_field);
+        groupId2TextView = (TextView) findViewById(R.id.group_id_field2);
         usernameField = (EditText) findViewById(R.id.username_field);
+        contentField = (EditText) findViewById(R.id.content_field);
     }
 
     @Override
@@ -56,6 +71,7 @@ public class GroupManagementActivity extends FragmentActivity implements OnClick
                             currentGroup.save();
                         } catch (GroupOperationException e) {
                             e.printStackTrace();
+                            currentGroup = null;
                             has_exception = true;
                         }
                         return null;
@@ -65,6 +81,7 @@ public class GroupManagementActivity extends FragmentActivity implements OnClick
                     protected void onPostExecute(Void result) {
                         super.onPostExecute(result);
                         groupIdTextView.setText(currentGroup.getGroupName());
+                        fetchGroupNote();
                     }
                 }.execute();
                 break;
@@ -101,6 +118,7 @@ public class GroupManagementActivity extends FragmentActivity implements OnClick
                                 public void onClick(DialogInterface dialog, int which) {
                                     currentGroup = groups.get(which);
                                     groupIdTextView.setText(currentGroup.getGroupName());
+                                    fetchGroupNote();
                                 }
                             });
                             ab.setNegativeButton(android.R.string.cancel, null);
@@ -111,7 +129,7 @@ public class GroupManagementActivity extends FragmentActivity implements OnClick
                 break;
             case R.id.add_to_group: {
                 if (currentGroup == null) {
-                    Toast.makeText(this, "Create or select a group first", Toast.LENGTH_LONG)
+                    Toast.makeText(this, R.string.tip_get_group_first, Toast.LENGTH_LONG)
                             .show();
                     return;
                 }
@@ -155,7 +173,7 @@ public class GroupManagementActivity extends FragmentActivity implements OnClick
                 break;
             case R.id.list_group_members: {
                 if (currentGroup == null) {
-                    Toast.makeText(this, "Create or select a group first", Toast.LENGTH_LONG)
+                    Toast.makeText(this, R.string.tip_get_group_first, Toast.LENGTH_LONG)
                             .show();
                     return;
                 }
@@ -166,7 +184,7 @@ public class GroupManagementActivity extends FragmentActivity implements OnClick
                     protected Void doInBackground(Void... params) {
                         try {
                             members = currentGroup.listMembers();
-                            for (KiiUser u:members) {
+                            for (KiiUser u : members) {
                                 u.refresh();
                             }
                         } catch (Exception e) {
@@ -200,6 +218,129 @@ public class GroupManagementActivity extends FragmentActivity implements OnClick
                 }.execute();
             }
                 break;
+            case R.id.select_belonged_group: {
+                new AsyncTaskWithProgress(this) {
+                    List<KiiGroup> groups = null;
+
+                    @Override
+                    protected Void doInBackground(Void... params) {
+                        try {
+                            groups = KiiUser.getCurrentUser().memberOfGroups();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            has_exception = true;
+                        }
+                        return null;
+                    }
+
+                    @Override
+                    protected void onPostExecute(Void result) {
+                        super.onPostExecute(result);
+                        if (groups == null || groups.size() == 0) {
+                            Toast.makeText(mActivity, "You don't belong to any groups, create one.",
+                                    Toast.LENGTH_LONG).show();
+                        } else {
+                            String[] items = new String[groups.size()];
+                            for (int i = 0; i < groups.size(); i++) {
+                                items[i] = groups.get(i).getGroupName();
+                            }
+                            AlertDialog.Builder ab = new AlertDialog.Builder(mActivity);
+                            ab.setTitle(R.string.choose_group);
+                            ab.setItems(items, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    currentGroup2 = groups.get(which);
+                                    groupId2TextView.setText(currentGroup2.getGroupName());
+                                    fetchGroupNote();
+                                }
+                            });
+                            ab.setNegativeButton(android.R.string.cancel, null);
+                            ab.show();
+                        }
+                    }
+                }.execute();
+            }
+                break;
+            case R.id.set_group_content: {
+                if (currentGroup2 == null) {
+                    Toast.makeText(this, R.string.tip_get_mem_group_first, Toast.LENGTH_LONG)
+                            .show();
+                    return;
+                }
+                final String text = contentField.getText().toString().trim();
+                if (TextUtils.isEmpty(text)) {
+                    Toast.makeText(this, "Input some text in above field first", Toast.LENGTH_LONG)
+                            .show();
+                    return;
+                }
+                if (group_content == null) {
+                    Toast.makeText(this, "Try to select group again", Toast.LENGTH_LONG)
+                            .show();
+                    return;
+                }
+                new AsyncTaskWithProgress(this) {
+
+                    @Override
+                    protected Void doInBackground(Void... params) {
+                        try {
+                            group_content.set(NOTE_FIELD_NAME, text);
+                            group_content.save();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            group_content = null;
+                            has_exception = true;
+                        }
+                        return null;
+                    }
+
+                    @Override
+                    protected void onPostExecute(Void result) {
+                        super.onPostExecute(result);
+                        if (!has_exception) {
+                            Toast.makeText(mActivity, "Change group note successfully",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }.execute();
+            }
+                break;
         }
     }
+
+    protected void fetchGroupNote() {
+        new AsyncTaskWithProgress(this) {
+
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+                    KiiBucket bucket = currentGroup2.bucket(BUCKET_NAME);
+                    KiiQuery all_query = new KiiQuery();
+                    KiiQueryResult<KiiObject> result = bucket.query(all_query);
+
+                    List<KiiObject> objLists = result.getResult();
+                    if (objLists.size() > 0) {
+                        group_content = objLists.get(0);
+                        group_content.refresh();
+                    } else {
+                        group_content = bucket.object();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    group_content = null;
+                    has_exception = true;
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void result) {
+                super.onPostExecute(result);
+                if (group_content != null) {
+                    contentField.setHint("Not set yet");
+                    contentField.setText(group_content.getString(NOTE_FIELD_NAME, ""));
+                }
+            }
+        }.execute();
+    }
+
 }
